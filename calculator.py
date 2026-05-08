a86d92e31c2a077175eafd6d53519578d5103b26
kheum sokkhen <sokkhenkheum7@gmail.com>
2026-05-08 12:46:12 +0700

    close Sub

--- calculator.py
+++ calculator.py
@@ -46,11 +46,11 @@ def Add():
     
     
     
-def Sub():
-    a=int(First_Value.get())
-    b=int(Second_Value.get())
-    Sub=a-b
-    result_label.config(text = "Operation Result is :  " +str(Sub))
+# def Sub():
+#     a=int(First_Value.get())
+#     b=int(Second_Value.get())
+#     Sub=a-b
+#     result_label.config(text = "Operation Result is :  " +str(Sub))
     
     
 def Mul():
